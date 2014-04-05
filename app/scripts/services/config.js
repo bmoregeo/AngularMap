9c0e46800d4f86f95de0b4ad3205165cf7d56889
christopherfricke <christopher.fricke@bmoregeo.com>
2014-04-05 09:08:08 -0400

    Added Config

---
+++ app/scripts/services/config.js
@@ -0,0 +1,51 @@
+'use strict';
+
+angular.module('mapAppApp')
+  .factory('Config', [function Config() {
+    return {
+        title: 'Simple Leaflet Viewer',
+        map: {
+            initialCenter:{
+                lat: 37.270833,
+                lng: -76.706944,
+                zoom: 12
+              },
+              baselayers:{
+                  osm: {
+                    name: 'Open Street Map',
+                    url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    type: 'xyz'
+                  },
+                  cloudmade: {
+                    name: 'Cloudmade Tourist',
+                    type: 'xyz',
+                    url: 'http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png',
+                    layerParams: {
+                        key: '007b9471b4c74da4a6ec7ff43552b16f',
+                        styleId: 7
+                      }
+                    }
+                  },
+              overlays: {
+                  buildings: {
+                      name:'Buildings',
+                      type: 'geoJSON',
+                      url:'http://tile.openstreetmap.us/vectiles-buildings/{z}/{x}/{y}.json',
+                      layerOptions: {
+                          style: {
+                              color: "#00D",
+                              fillColor: "#00D",
+                              weight: 1.0,
+                              opacity: 0.6,
+                              fillOpacity: .2
+                            }
+                        },
+                      pluginOptions:{
+                          cliptiles: true
+                      }
+                    }
+                }
+                }
+              };
+
+  }]);
